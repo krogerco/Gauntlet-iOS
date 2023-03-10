4bc6195b721659e18136b7738e6a1958f60fedc5
David Barry <david.barry@kroger.com>
2023-03-09 16:02:39 -0800

    Update year in license header.

--- Source/Gauntlet/Operators/FailureReasonOperators.swift
+++ Source/Gauntlet/Operators/FailureReasonOperators.swift
@@ -3,7 +3,7 @@
 //
 //  MIT License
 //
-//  Copyright (c) [2020] The Kroger Co. All rights reserved.
+//  Copyright (c) [2023] The Kroger Co. All rights reserved.
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
